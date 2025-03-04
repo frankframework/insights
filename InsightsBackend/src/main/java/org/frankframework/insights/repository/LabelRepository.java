77866f7b6598d7aa7aa792f98437d12152ab1632
stijnpotters <stijn.potters1@gmail.com>
2025-03-04 11:52:10 +0100

    changed repository id's to string datatype

--- InsightsBackend/src/main/java/org/frankframework/insights/repository/LabelRepository.java
+++ InsightsBackend/src/main/java/org/frankframework/insights/repository/LabelRepository.java
@@ -7,6 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LabelRepository extends JpaRepository<Label, UUID> {
-    Set<Label> findByNameIn(Set<String> names);
+public interface LabelRepository extends JpaRepository<Label, String> {
 }
--- InsightsBackend/src/main/java/org/frankframework/insights/repository/MilestoneRepository.java
+++ InsightsBackend/src/main/java/org/frankframework/insights/repository/MilestoneRepository.java
@@ -1,9 +1,8 @@
 package org.frankframework.insights.repository;
 
 import java.util.UUID;
-import org.frankframework.insights.models.Issue;
 import org.frankframework.insights.models.Milestone;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MilestoneRepository extends JpaRepository<Milestone, UUID> {}
+public interface MilestoneRepository extends JpaRepository<Milestone, String> {}
