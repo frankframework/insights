d5691e8296cfd9c21059e5b88dd1310e5768de7b
stijnpotters <stijn.potters1@gmail.com>
2025-03-07 11:13:13 +0100

    adjusted commit components to connect them to branches

--- InsightsBackend/src/main/java/org/frankframework/insights/commit/Commit.java
+++ InsightsBackend/src/main/java/org/frankframework/insights/commit/Commit.java
@@ -2,6 +2,8 @@ package org.frankframework.insights.commit;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+import java.time.OffsetDateTime;
+import java.util.Set;
 import lombok.Getter;
 import org.frankframework.insights.pullrequest.PullRequest;
 
@@ -18,6 +20,12 @@ public class Commit {
 
     @Column(nullable = false)
     private String message;
+
+    @Column(name = "timestamp", columnDefinition = "TIMESTAMP")
+    private OffsetDateTime timestamp;
+
+    @ManyToMany(mappedBy = "commits")
+    private Set<Branch> branches;
 
     @ManyToOne
     private PullRequest pullRequest;
