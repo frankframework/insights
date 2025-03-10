d040c5e1b8d00c0c7a376fec4832e8b2eb6c36e1
stijnpotters <stijn.potters1@gmail.com>
2025-03-10 14:07:28 +0100

    added logging and exception handling for branches, commits and releases

--- InsightsBackend/src/main/java/org/frankframework/insights/service/BranchService.java
+++ InsightsBackend/src/main/java/org/frankframework/insights/service/BranchService.java
@@ -2,52 +2,79 @@ package org.frankframework.insights.service;
 
 import java.util.List;
 import java.util.Set;
+
+import lombok.extern.slf4j.Slf4j;
 import org.frankframework.insights.clients.GitHubClient;
 import org.frankframework.insights.dto.BranchDTO;
-import org.frankframework.insights.mapper.BranchMapper;
+import org.frankframework.insights.exceptions.branches.BranchDatabaseException;
+import org.frankframework.insights.exceptions.branches.BranchInjectionException;
+import org.frankframework.insights.mapper.Mapper;
 import org.frankframework.insights.models.Branch;
 import org.frankframework.insights.repository.BranchRepository;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class BranchService {
     private final GitHubClient gitHubClient;
-    private final BranchMapper branchMapper;
+    private final Mapper branchMapper;
     private final BranchRepository branchRepository;
 
-    public BranchService(GitHubClient gitHubClient, BranchMapper branchMapper, BranchRepository branchRepository) {
+    public BranchService(GitHubClient gitHubClient, Mapper branchMapper, BranchRepository branchRepository) {
         this.gitHubClient = gitHubClient;
         this.branchMapper = branchMapper;
         this.branchRepository = branchRepository;
     }
 
-    public void injectBranches() throws RuntimeException {
+    public void injectBranches() throws BranchInjectionException {
         if (!branchRepository.findAll().isEmpty()) {
+            log.info("Branches already found in the in database");
             return;
         }
 
         try {
+            log.info("Start injecting GitHub branches");
             Set<BranchDTO> branchDTOs = gitHubClient.getBranches();
-            Set<Branch> branches = branchMapper.toEntity(branchDTOs);
+            Set<Branch> branches = branchMapper.toEntity(branchDTOs, Branch.class);
             saveBranches(branches);
         } catch (Exception e) {
-            throw new RuntimeException("Error fetching commits", e);
+            throw new BranchInjectionException("Error while injecting GitHub branches", e);
         }
     }
 
-    public boolean doesBranchContainCommit(Branch branch, String commitOid) {
-        return branch.getCommits().stream().anyMatch(commit -> commit.getOid().equals(commitOid));
+    public boolean doesBranchContainCommit(Branch branch, String commitOid) throws BranchDatabaseException {
+        try {
+            boolean containsCommit = branch.getCommits().stream()
+                    .anyMatch(commit -> commit.getOid().equals(commitOid));
+
+            log.info("Branch {} contains commit: {}", branch.getName(), containsCommit);
+
+            return containsCommit;
+
+        } catch (Exception e) {
+            log.error("Error while checking if branch contains commit: {}", commitOid, e);
+            throw new BranchDatabaseException("Error while checking if branch contains the commit hash", e);
+        }
     }
 
     public List<Branch> getAllBranches() {
         return branchRepository.findAll();
     }
 
-    public void saveOrUpdateBranch(Branch branch) {
-        branchRepository.save(branch);
+    public void saveOrUpdateBranch(Branch branch) throws BranchDatabaseException {
+        try {
+            branchRepository.save(branch);
+            log.info("Successfully updated and saved branches");
+        } catch (Exception e) {
+            throw new BranchDatabaseException("Error while updating and saving branches", e);
+        }
     }
 
-    private void saveBranches(Set<Branch> branches) {
-        branchRepository.saveAll(branches);
+    private void saveBranches(Set<Branch> branches) throws BranchDatabaseException {
+        try {
+            branchRepository.saveAll(branches);
+        } catch (Exception e) {
+            throw new BranchDatabaseException("Error while saving set of branches", e);
+        }
     }
 }
