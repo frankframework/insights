07644130f51cdb3f26363adaa8d80a90bfc6066c
stijnpotters <stijn.potters1@gmail.com>
2025-03-14 15:15:51 +0100

    added commit logics

--- InsightsBackend/src/main/java/org/frankframework/insights/commit/Commit.java
+++ InsightsBackend/src/main/java/org/frankframework/insights/commit/Commit.java
@@ -2,13 +2,19 @@ package org.frankframework.insights.commit;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+import java.time.OffsetDateTime;
+import java.util.HashSet;
+import java.util.Set;
 import lombok.Getter;
+import lombok.Setter;
+import org.frankframework.insights.branch.Branch;
 import org.frankframework.insights.pullrequest.PullRequest;
 
 @Entity
 @Table(name = "commit")
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Getter
+@Setter
 public class Commit {
     @Id
     private String id;
@@ -16,9 +22,18 @@ public class Commit {
     @Column(nullable = false, unique = true)
     private String sha;
 
+    private String oid;
+
+    @Lob
     @Column(nullable = false)
     private String message;
 
-    @ManyToOne
+    @Column(name = "committed_date", columnDefinition = "TIMESTAMP")
+    private OffsetDateTime committedDate;
+
+    @ManyToMany(mappedBy = "commits")
+    private Set<Branch> branches = new HashSet<>();
+
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
     private PullRequest pullRequest;
 }
---
+++ InsightsBackend/src/main/java/org/frankframework/insights/commit/CommitRepository.java
@@ -0,0 +1,11 @@
+package org.frankframework.insights.commit;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommitRepository extends JpaRepository<Commit, String> {
+    @Query("SELECT COUNT(bc) FROM Branch b JOIN b.commits bc")
+    int countAllBranchCommitRelations();
+}
