dcada254de5c4c801e63b0fbcbe02c18b1faa3c0
stijnpotters <stijn.potters1@gmail.com>
2025-04-18 10:00:44 +0200

    optimized the code and refactored some bugs

--- InsightsBackend/src/main/java/org/frankframework/insights/milestone/MilestoneResponse.java
+++ InsightsBackend/src/main/java/org/frankframework/insights/milestone/MilestoneResponse.java
@@ -1,5 +1,8 @@
 package org.frankframework.insights.milestone;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import org.frankframework.insights.github.GitHubPropertyState;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record MilestoneResponse(String id, int number, GitHubPropertyState state, String title) {}
