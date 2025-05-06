57a40ae397fdd7a74d82ff94556e6c70bc5ae648
stijnpotters <stijn.potters1@gmail.com>
2025-05-06 15:51:31 +0200

    added all services with methods

---
+++ InsightsFrontend/src/app/services/issue.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import {ApiResponse, AppService, GitHubState} from "../app.service";
+import {Milestone} from "./milestone.service";
+import {Label} from "./label.service";
+import {Observable} from "rxjs";
+
+export type Issue = {
+	id: string;
+	number: number;
+	title: string;
+	state: GitHubState;
+	closedAt: Date;
+	url: string;
+	businessValue: string;
+	milestone: Milestone;
+	labels: Label[];
+	parentIssue: Issue;
+	subIssues: Issue[];
+}
+
+export type TimeSpan = {
+	startDate: string;
+	endDate: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IssueService {
+
+	constructor(private appService: AppService) { }
+
+	getIssuesByReleaseId(releaseId: string): Observable<Record<string, ApiResponse<Issue[]>>> {
+		return this.appService.getAll<Issue[]>(this.appService.createAPIUrl("/issues/release/" + releaseId));
+	}
+
+	getIssuesByMilestoneId(milestoneId: string): Observable<Record<string, ApiResponse<Issue[]>>> {
+		return this.appService.getAll<Issue[]>(this.appService.createAPIUrl("/issues/milestone/" + milestoneId));
+	}
+
+	getIssuesByTimeSpan(startDate: Date, endDate: Date): Observable<Record<string, ApiResponse<Issue[]>>> {
+		const url = this.appService.createAPIUrl("issues/timespan");
+
+		const timestampParams: TimeSpan = {
+			startDate: startDate.toISOString(),
+			endDate: endDate.toISOString()
+		};
+
+		return this.appService.getAll<Issue[]>(url, timestampParams);
+	}
+
+}
--- InsightsFrontend/src/main.ts
+++ InsightsFrontend/src/main.ts
@@ -3,10 +3,18 @@ import { AppComponent } from './app/app.component';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import {HTTP_INTERCEPTORS, provideHttpClient} from '@angular/common/http';
+import {HttpInterceptorService} from "./app/services/http-interceptor.service";
 
 bootstrapApplication(AppComponent, {
 	providers: [
 		provideRouter(routes),
-		provideAnimations()
+		provideAnimations(),
+		provideHttpClient(),
+		{
+			provide: HTTP_INTERCEPTORS,
+			useClass: HttpInterceptorService,
+			multi: true
+		}
 	]
 }).catch(err => console.error(err));
