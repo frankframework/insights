913fee84ccedcae672c1360a3f86350a1183675a
stijnpotters <stijn.potters1@gmail.com>
2025-05-06 15:51:31 +0200

    added all services with methods

---
+++ InsightsFrontend/src/app/services/http-interceptor.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {
+	HttpEvent, HttpInterceptor, HttpHandler, HttpRequest
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class HttpInterceptorService implements HttpInterceptor {
+	intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+
+		const requestWithHeaders = req.clone({
+			setHeaders: {
+				'Content-Type': 'application/json'
+			}
+		});
+
+		return next.handle(requestWithHeaders);
+	}
+}
