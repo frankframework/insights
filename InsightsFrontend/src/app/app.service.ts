ea2827fa2a521d565bdaa61c546648222f3e1816
stijnpotters <stijn.potters1@gmail.com>
2025-06-10 16:56:29 +0200

    changed default backendUrl in appService.ts to production

--- InsightsFrontend/src/app/app.service.ts
+++ InsightsFrontend/src/app/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { TimeSpanParameters } from './services/issue.service';
-import { environment } from '../environments/environment.development';
+import { environment } from '../environments/environment';
 
 export const GitHubStates = {
   OPEN: 0,
