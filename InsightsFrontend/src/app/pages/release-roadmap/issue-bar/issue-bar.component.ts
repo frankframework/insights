e59ac35d52ead2ca1835d640465d1846a92b2de6
stijnpotters <stijn.potters1@gmail.com>
2025-06-25 13:14:42 +0200

    Refined release roadmap to second version

--- InsightsFrontend/src/app/pages/release-roadmap/issue-bar/issue-bar.component.ts
+++ InsightsFrontend/src/app/pages/release-roadmap/issue-bar/issue-bar.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Issue } from '../../../services/issue.service';
 import { GitHubStates } from '../../../app.service';
+import { Issue } from '../../../services/issue.service';
 
 @Component({
   selector: 'app-issue-bar',
@@ -24,22 +24,20 @@ export class IssueBarComponent implements OnInit {
   private setPriorityStyle(): void {
     if (this.isClosed) {
       this.priorityStyle = {
-        'background-color': '#ede9fe',
-        color: '#5b21b6',
-        'border-color': '#c4b5fd',
+        'background-color': '#f3e8ff',
+        color: '#581c87',
+        'border-color': '#d8b4fe',
       };
       return;
     }
-
     const color = this.issue.issuePriority?.color;
     const defaultStyles = { 'background-color': '#e5e7eb', color: '#4b5563', 'border-color': '#d1d5db' };
-
     this.priorityStyle =
       color && /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(color)
         ? {
-            'background-color': `${color}25`,
-            color: color,
-            'border-color': color,
+            'background-color': `#${color}25`,
+            color: `#${color}`,
+            'border-color': `#${color}`,
           }
         : defaultStyles;
   }
