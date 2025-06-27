7a168327128ca455dc029df2a6a8d22f56d2015c
stijnpotters <stijn.potters1@gmail.com>
2025-06-27 10:10:33 +0200

    hotfix: manually fixed package-lock.json inconsistency

--- InsightsFrontend/cypress/e2e/routing.cy.ts
+++ InsightsFrontend/cypress/e2e/routing.cy.ts
@@ -1,7 +1,7 @@
 describe('Application Routing', () => {
   beforeEach(() => {
     cy.visit('/');
-    cy.get('app-tab-header').find('li').contains('Roadmap').click();
+    cy.get('app-header').find('li').contains('Roadmap').click();
   });
 
   it('should navigate between Graph and Roadmap pages using the tab header', () => {
@@ -9,7 +9,7 @@ describe('Application Routing', () => {
     cy.get('app-roadmap').should('be.visible');
     cy.get('app-release-graph').should('not.exist');
 
-    cy.get('app-tab-header').find('li').contains('Release graph').click();
+    cy.get('app-header').find('li').contains('Release graph').click();
     cy.url().should('not.include', '/roadmap');
     cy.get('app-release-graph').should('be.visible');
     cy.get('app-roadmap').should('not.exist');
