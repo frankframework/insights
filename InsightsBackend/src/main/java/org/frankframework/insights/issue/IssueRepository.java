fa1c5bed0dab828e68a2d76e2e2e2cdbf1d0d5be
stijnpotters <stijn.potters1@gmail.com>
2025-08-26 13:55:32 +0200

    fixed checkstyle violation

--- InsightsBackend/src/main/java/org/frankframework/insights/issue/IssueRepository.java
+++ InsightsBackend/src/main/java/org/frankframework/insights/issue/IssueRepository.java
@@ -1,6 +1,5 @@
 package org.frankframework.insights.issue;
 
-import java.time.OffsetDateTime;
 import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
