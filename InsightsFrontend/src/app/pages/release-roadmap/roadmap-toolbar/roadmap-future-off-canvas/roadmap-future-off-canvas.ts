e6c0f388ed915173e10a8c4bf00984eeaecec176
stijnpotters <stijn.potters1@gmail.com>
2025-08-28 16:14:26 +0200

    initial future plans off canvas

---
+++ InsightsFrontend/src/app/pages/release-roadmap/roadmap-toolbar/roadmap-future-off-canvas/roadmap-future-off-canvas.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit, inject, ChangeDetectorRef, OnDestroy, Output, EventEmitter } from '@angular/core';
+import { LoaderComponent } from '../../../../components/loader/loader.component';
+import { Issue, IssueService } from '../../../../services/issue.service';
+import { OffCanvasComponent } from '../../../../components/off-canvas/off-canvas.component';
+import { Subject, catchError, of, takeUntil } from 'rxjs';
+import { FutureEpic } from './future-epic/future-epic';
+
+@Component({
+  selector: 'app-roadmap-future-off-canvas',
+  standalone: true,
+  imports: [LoaderComponent, OffCanvasComponent, FutureEpic],
+  templateUrl: './roadmap-future-off-canvas.html',
+  styleUrl: './roadmap-future-off-canvas.scss',
+})
+export class RoadmapFutureOffCanvasComponent implements OnInit, OnDestroy {
+  @Output() closeCanvas = new EventEmitter<void>();
+
+  public readonly OFF_CANVAS_NAME = 'Future Plans';
+
+  public futureEpicIssues?: Issue[];
+  public isLoading = true;
+
+  private readonly destroy$ = new Subject<void>();
+  private readonly issueService = inject(IssueService);
+  private readonly cdr = inject(ChangeDetectorRef);
+
+  public ngOnInit(): void {
+    this.fetchData();
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  private fetchData(): void {
+    this.isLoading = true;
+    this.cdr.markForCheck();
+
+    this.issueService
+      .getFutureEpicIssues()
+      .pipe(
+        takeUntil(this.destroy$),
+        catchError((error) => {
+          console.error('Failed to load future plans:', error);
+          return of();
+        }),
+      )
+      .subscribe({
+        next: (issues: Issue[]) => {
+          this.futureEpicIssues = issues && issues.length > 0 ? issues : undefined;
+        },
+        complete: () => {
+          this.isLoading = false;
+          this.cdr.markForCheck();
+        },
+      });
+  }
+}
--- InsightsFrontend/src/app/pages/release-roadmap/roadmap-toolbar/roadmap-toolbar.component.ts
+++ InsightsFrontend/src/app/pages/release-roadmap/roadmap-toolbar/roadmap-toolbar.component.ts
@@ -1,15 +1,29 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BehaviorSubject } from 'rxjs';
+import { RoadmapFutureOffCanvasComponent } from './roadmap-future-off-canvas/roadmap-future-off-canvas';
 
 @Component({
   selector: 'app-roadmap-toolbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RoadmapFutureOffCanvasComponent, RoadmapFutureOffCanvasComponent],
   templateUrl: './roadmap-toolbar.component.html',
   styleUrls: ['./roadmap-toolbar.component.scss'],
 })
 export class RoadmapToolbarComponent {
-  @Input() periodLabel = '';
-  @Output() changePeriod = new EventEmitter<number>();
-  @Output() resetPeriod = new EventEmitter<void>();
+  @Input() public periodLabel = '';
+  @Output() public readonly changePeriod = new EventEmitter<number>();
+  @Output() public readonly resetPeriod = new EventEmitter<void>();
+
+  private readonly _isCanvasVisible = new BehaviorSubject<boolean>(false);
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  public readonly isCanvasVisible$ = this._isCanvasVisible.asObservable();
+
+  public openFutureDetails(): void {
+    this._isCanvasVisible.next(true);
+  }
+
+  public closeFutureDetails(): void {
+    this._isCanvasVisible.next(false);
+  }
 }
