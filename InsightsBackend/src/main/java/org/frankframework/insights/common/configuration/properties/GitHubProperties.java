5bc818ea5b4240a11f809a9a61a4fa034d1e5fcf
stijnpotters <stijn.potters1@gmail.com>
2025-09-04 12:31:03 +0200

    adjusted code with review improvements

--- InsightsBackend/src/main/java/org/frankframework/insights/common/configuration/properties/GitHubProperties.java
+++ InsightsBackend/src/main/java/org/frankframework/insights/common/configuration/properties/GitHubProperties.java
@@ -15,4 +15,10 @@ public class GitHubProperties {
     private List<String> branchProtectionRegexes;
     private List<String> includedLabels;
     private Boolean fetch;
+
+	public List<String> getIncludedLabels() {
+		return includedLabels.stream()
+				.map(String::toUpperCase)
+				.toList();
+	}
 }
