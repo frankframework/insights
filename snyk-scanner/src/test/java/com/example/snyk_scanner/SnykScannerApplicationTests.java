39f14e81f511fab4b878d0952de938088d2a16dd
stijnpotters <stijn.potters1@gmail.com>
2025-09-17 14:52:39 +0200

    added snyk scanner sub module for scanning FF releases

---
+++ snyk-scanner/src/test/java/com/example/snyk_scanner/SnykScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.snyk_scanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnykScannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
