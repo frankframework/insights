9586ce08db6f80de2ad46595acc7ae0312481e11
stijnpotters <stijn.potters1@gmail.com>
2025-09-17 11:28:11 +0200

    changed string list to list with strings

--- src/main/java/org/frankframework/insights/common/configuration/CorsConfig.java
+++ src/main/java/org/frankframework/insights/common/configuration/CorsConfig.java
@@ -10,18 +10,18 @@ import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig {
-    private final String[] allowedOrigins;
+    private final List<String> allowedOrigins;
     private static final long MAX_CORS_CONFIGURATION_CONNECTION = 3600L;
 
     public CorsConfig(CorsProperties corsProperties) {
-        allowedOrigins = corsProperties.getOrigins();
+        this.allowedOrigins = corsProperties.getOrigins();
     }
 
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
         corsConfig.setAllowCredentials(true);
-        corsConfig.setAllowedOrigins(List.of(allowedOrigins));
+        corsConfig.setAllowedOrigins(allowedOrigins);
         corsConfig.setAllowedMethods(List.of("GET"));
         corsConfig.setAllowedHeaders(List.of("Origin", "Content-Type", "Accept"));
         corsConfig.setMaxAge(MAX_CORS_CONFIGURATION_CONNECTION);
--- src/main/java/org/frankframework/insights/common/configuration/properties/CorsProperties.java
+++ src/main/java/org/frankframework/insights/common/configuration/properties/CorsProperties.java
@@ -4,9 +4,11 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.List;
+
 @ConfigurationProperties(prefix = "cors.allowed")
 @Getter
 @Setter
 public class CorsProperties {
-    private String[] origins;
+    private List<String> origins;
 }
