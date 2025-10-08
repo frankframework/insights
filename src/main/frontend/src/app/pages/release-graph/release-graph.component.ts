4fa840d9e0ecca00e8ddf1d1156d7b0d0f48fd96
stijnpotters <stijn.potters1@gmail.com>
2025-10-08 11:08:28 +0200

    added additional y offset for the release graph

--- src/main/frontend/src/app/pages/release-graph/release-graph.component.ts
+++ src/main/frontend/src/app/pages/release-graph/release-graph.component.ts
@@ -353,7 +353,7 @@ export class ReleaseGraphComponent implements OnInit, OnDestroy {
     this.scale = targetHeight / Math.max(graphH, 1);
     const scaledGraphH = graphH * this.scale;
     const topPadding = (H - scaledGraphH) / 2;
-    this.translateY = -minY * this.scale + topPadding;
+    this.translateY = -minY * this.scale + topPadding + 40;
 
     this.maxTranslateX = -minX * this.scale + W * 0.2;
     this.minTranslateX = W - maxX * this.scale - W * 0.45;
