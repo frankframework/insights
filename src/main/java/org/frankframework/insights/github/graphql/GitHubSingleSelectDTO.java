463903bebbd5dad749ffe9c6c8ff73ab46433ff1
stijnpotters <stijn.potters1@gmail.com>
2025-11-13 14:29:21 +0100

    added redirection tests

--- src/main/java/org/frankframework/insights/github/graphql/GitHubSingleSelectDTO.java
+++ src/main/java/org/frankframework/insights/github/graphql/GitHubSingleSelectDTO.java
@@ -4,5 +4,5 @@ import java.util.List;
 import org.frankframework.insights.common.client.graphql.GraphQLPageInfoDTO;
 
 public record GitHubSingleSelectDTO(List<SingleSelectObject> nodes, GraphQLPageInfoDTO pageInfo) {
-	public record SingleSelectObject(String name, List<GitHubSingleSelectProjectItemDTO> options) {}
+    public record SingleSelectObject(String name, List<GitHubSingleSelectProjectItemDTO> options) {}
 }
