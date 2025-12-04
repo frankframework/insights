aba63b605f1d96096e50bac1365e2852b81595e1
stijnpotters <stijn.potters1@gmail.com>
2025-12-04 18:43:47 +0100

    ignore properties that are not in the release  entity to keep the lastScanned property inside the system

--- src/main/java/org/frankframework/insights/release/ReleaseResponse.java
+++ src/main/java/org/frankframework/insights/release/ReleaseResponse.java
@@ -1,7 +1,11 @@
 package org.frankframework.insights.release;
 
 import java.time.OffsetDateTime;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import org.frankframework.insights.branch.BranchResponse;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record ReleaseResponse(
         String id, String tagName, String name, OffsetDateTime publishedAt, String commitSha, BranchResponse branch) {}
