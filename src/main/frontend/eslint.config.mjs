ec07de0aaec58d3f11c10c53e73c6b18b179215e
stijnpotters <stijn.potters1@gmail.com>
2026-08-10 11:09:41 +0200

    fix: change prefer-on-push-component-change-detection rule from error to warn in eslint configuration

--- src/main/frontend/eslint.config.mjs
+++ src/main/frontend/eslint.config.mjs
@@ -51,7 +51,7 @@ export default [
       '@angular-eslint/directive-selector': ['error', { type: 'attribute', prefix: 'app', style: 'camelCase' }],
       '@angular-eslint/component-selector': ['error', { type: 'element', prefix: 'app', style: 'kebab-case' }],
 
-      '@angular-eslint/prefer-on-push-component-change-detection': 'error',
+      '@angular-eslint/prefer-on-push-component-change-detection': 'warn',
       '@angular-eslint/prefer-signals': 'error',
       '@angular-eslint/prefer-output-emitter-ref': 'error',
       '@angular-eslint/no-input-rename': 'error',
